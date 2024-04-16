b960071c2578016006699120c54ff879fd075371
r0bb3ry <jblgg@yandex.ru>
2024-04-17 00:22:52 +0300

    notebook v1.1

---
+++ src/main/java/com/example/popshop1/Popshop1Application.java
@@ -0,0 +1,13 @@
+package com.example.popshop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Popshop1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Popshop1Application.class, args);
+    } //notebookv1
+
+}
---
+++ src/main/java/com/example/popshop1/services/ProductService.java
@@ -0,0 +1,36 @@
+package com.example.popshop1.services;
+
+import com.example.popshop1.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+    private long ID = 0;
+
+    {
+        products.add(new Product(++ID,"Iphone","SmartPhone",61236));
+        products.add(new Product(++ID,"Copmuter","SmartComputer",100006));
+    }
+
+    public List<Product> listProducts() { return products; }
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public Product getProductById(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) return product;
+        }
+        return null;
+    }
+}
