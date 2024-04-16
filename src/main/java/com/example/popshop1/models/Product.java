98313405fbf887d03ac85452fe8995e0ad7a279c
robbery999 <jblgg@yandex.ru>
2024-04-16 19:59:17 +0300

    vers1.1

---
+++ src/main/java/com/example/popshop1/models/Product.java
@@ -0,0 +1,14 @@
+package com.example.popshop1.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String title;
+    private String description;
+    private int price;
+
+}
---
+++ src/test/java/com/example/popshop1/Popshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popshop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Popshop1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
